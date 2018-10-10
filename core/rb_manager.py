a45df82dcbf2bd7fd87ecacd6edff562959b0dd9
Niccolo Dal Santo <dalsantolocal@mathicsepc76.epfl.ch>
2018-10-10 13:22:38 +0200

    verified and tested code for SVD and building the basis

--- core/rb_manager.py
+++ core/rb_manager.py
@@ -18,7 +18,103 @@ class RbManager( ):
     def __init__( self ):
         return
 
+    def import_snapshots_matrix( self, _input_file ):
+        self.M_snapshots_matrix = np.loadtxt( _input_file )
+        self.M_ns = self.M_snapshots_matrix.shape[1]
+        return 
+
+    def import_test_snapshots_matrix( self, _input_file ):
+        self.M_test_snapshots_matrix = np.loadtxt( _input_file )
+        self.M_ns_test = self.M_test_snapshots_matrix.shape[1]
+        return 
+    
+    def get_snapshots_matrix( self ):
+        return self.M_snapshots_matrix
+    
+    def get_number_of_snapshots( self ):
+        return self.M_ns
+    
+    def get_basis( self ):
+        return self.M_basis
+    
+    def get_number_of_basis( self ):
+        return self.M_N
+    
+    def print_snapshots_matrix( self ):
+        
+        print( "The snapshots matrix is: \n" )
+        print( self.M_snapshots_matrix )
+        
+        return
+    
+    def set_random_snapshots_matrix( self, _rows = 4, _cols = 2 ):
+
+        self.M_snapshots_matrix = np.random.randn( _rows, _cols )
+        
+        return 
+    
+    def perform_pod( self, _tol = 10**(-5) ):
+        
+        U, s, V = np.linalg.svd( self.M_snapshots_matrix, full_matrices=False )
+        
+        total_energy = np.dot( s, np.transpose(s) )
+        
+        print( "The total energy of the field is %g" % total_energy )
+        
+        self.M_N = 0
+        
+        cumulative_energy = 0.0
+        record_cumulative_energy = np.ones( self.M_ns )
+        
+        while cumulative_energy / total_energy < 1 - _tol**2:
+            print( "Now N is %d and the cumulative energy is %g --- %g --- current sv %g " % ( self.M_N, cumulative_energy, cumulative_energy / total_energy, s[ self.M_N ] ) )
+            record_cumulative_energy[ self.M_N ] = cumulative_energy
+            cumulative_energy = cumulative_energy + s[ self.M_N ]*s[ self.M_N ]  # add the energy of next basis
+            self.M_N = self.M_N + 1                                       # add a basis in the count
+        
+        print( "The number of selected RB functions is %d" % self.M_N )
+
+        self.M_basis = U[:, 0:self.M_N]
+            
+        return
+
+    M_ns = 0
     M_snapshots_matrix = np.zeros( 0 )
+    M_ns_test = 0
+    M_test_snapshots_matrix = np.zeros( 0 )
+
+    M_N = 0
+    M_basis = np.zeros( 0 )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- examples/snapshots_importer/main.py
+++ examples/snapshots_importer/main.py
@@ -7,11 +7,34 @@ Created on Wed Oct 10 10:24:47 2018
 @email : niccolo.dalsanto@epfl.ch
 """
 
+#%%
+
+import numpy as np
+
 import sys
 sys.path.insert(0, '../../core')
 
 import rb_manager as rm
 
 print( rm.__doc__ )
+
+
+my_rb_manager = rm.RbManager( )
+
+#my_rb_manager.set_random_snapshots_matrix( 4, 2 )
+#my_rb_manager.print_snapshots_matrix( )
+#my_rb_manager.perform_pod( )
+
+
+
+
+
+
+snapshots_file = 'train_snapshots_matrix_20_50.txt'
+
+my_rb_manager.import_snapshots_matrix( snapshots_file )
+print( "The new number of snapshots is %d " % my_rb_manager.get_number_of_snapshots( ) )
+my_rb_manager.perform_pod( )
+
 
 
