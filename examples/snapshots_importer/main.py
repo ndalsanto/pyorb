d98a96254e5665c76cea8c3f0697dfc77b76c85c
Niccolo Dal Santo <dalsantolocal@mathicsepc76.epfl.ch>
2018-10-10 10:36:09 +0200

    added first example

---
+++ examples/snapshots_importer/main.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 10 10:24:47 2018
+
+@author: Niccolo' Dal Santo
+@email : niccolo.dalsanto@epfl.ch
+"""
+
+import sys
+sys.path.insert(0, '../../core')
+
+import rb_manager as rm
+
+print( rm.__doc__ )
+
+
