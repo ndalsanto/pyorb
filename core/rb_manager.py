8264aed81efce0769aeb70d2f306bd6b8acb58b6
Niccolo Dal Santo <dalsantolocal@mathicsepc76.epfl.ch>
2018-10-10 10:35:44 +0200

    added first class aimed at being the RB manager

---
+++ core/rb_manager.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 10 10:22:53 2018
+
+@author: Niccolo' Dal Santo
+@email : niccolo.dalsanto@epfl.ch
+
+This is the module for handling RB methods in python.
+"""
+
+
+
+import numpy as np
+
+class RbManager( ):
+    
+    def __init__( self ):
+        return
+
+    M_snapshots_matrix = np.zeros( 0 )
+
+
+
