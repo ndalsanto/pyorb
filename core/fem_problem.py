80bc69877cdc2b27079ff198670c53c1c5442e39
Niccolo' Dal Santo <dalsantolocal@mathicsepc76.epfl.ch>
2018-10-11 13:57:44 +0200

    started to prepare interface

--- core/fem_problem.py
+++ core/fem_problem.py
@@ -8,15 +8,18 @@ Created on Thu Oct 11 12:02:21 2018
 """
 
 def default_theta_function( _param, _q ):
+
+    print( "You are using the default theta function, please provide specific ones for your problem " )
+    
     pass
 
 class fem_problem( ):
     
-    def __init__( ):
+    def __init__( self ):
         return
     
     def get_theta_a( self, _param, _q ):
-        return self.M_theta_a( self, _param, _q )
+        return self.M_theta_a( _param, _q )
     
     def get_theta_f( self, _param, _q ):
         return self.M_theta_f( _param, _q )
