1fb53a07355be60c13438d471a9f05bc28d255e3
Niccolo Dal Santo <dalsantolocal@mathicsepc76.epfl.ch>
2018-10-11 12:06:25 +0200

    using parameter handler class in main

--- examples/thermal_block/main.py
+++ examples/thermal_block/main.py
@@ -16,13 +16,22 @@ sys.path.insert(0, '../../core')
 
 import rb_manager as rm
 import affine_decomposition as ad
+import parameter_handler as ph
 
 print( rm.__doc__ )
 
 
+mu0_min = 1.0; mu0_max = 50.
+mu1_min = 1.0; mu1_max = 50.
+mu2_min = 1.0; mu2_max = 50.
+
+param_min = np.array([mu0_min, mu1_min, mu2_min])
+param_max = np.array([mu0_max, mu1_max, mu2_max])
+num_parameters = param_min.shape[0]
 
 
-
+my_parameter_handler = ph.Parameter_handler( )
+my_parameter_handler.assign_parameters_bounds( param_min, param_max )
 
 my_affine_decomposition = ad.AffineDecompositionHandler( )
 my_affine_decomposition.set_Q( 4, 1 )                   # number of affine terms
@@ -36,6 +45,7 @@ my_rb_manager.set_affine_decomposition_handler( my_affine_decomposition )
 snapshots_file = 'train_snapshots_matrix_20_50.txt'
 my_rb_manager.import_snapshots_matrix( snapshots_file )
 
+#my_rb_manager.set_save_basis_functions( True, "basis.txt" )
 my_rb_manager.build_rb_approximation( 10**(-4) )
 
 
