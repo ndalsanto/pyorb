c1ca72b8ffe14c6f4e8adabf9cbb09aa3680e679
Niccolo' Dal Santo <ncl.dalsanto@gmail.com>
2018-11-29 16:56:23 +0100

    updated mdeim methods

--- examples/non_affine_diffusion/test_mdeim.py
+++ examples/non_affine_diffusion/test_mdeim.py
@@ -10,11 +10,11 @@ Created on Mon Nov 12 10:56:42 2018
 import numpy as np
 
 import sys
-sys.path.insert(0, '../../core')
+sys.path.insert(0, '../../')
 print(sys.path)
 
 
-import manage_external_engine as mee
+import pyorb_core.tpl_managers.external_engine_manager as mee
 
 # playing around with engine manager 
 my_matlab_engine_manager = mee.external_engine_manager( 'matlab', '/usr/scratch/dalsanto/EPFL/DeepLearning/feamat' )
@@ -22,7 +22,7 @@ my_matlab_engine_manager.start_engine( )
 my_matlab_external_engine = my_matlab_engine_manager.get_external_engine( )
 
 
-import parameter_handler as ph
+import pyorb_core.pde_problem.parameter_handler as ph
 
 mu0_min = 0.4; mu0_max = 0.6
 mu1_min = 0.4; mu1_max = 0.6
@@ -51,16 +51,27 @@ fom_specifics = {
 
 my_ndp.configure_fom( my_matlab_external_engine, fom_specifics )
 
-import m_deim
+import pyorb_core.rb_library.m_deim as m_deim
 my_mdeim = m_deim.Mdeim( my_ndp )
 
+base_offline_folder = 'offline/'
+
+my_mdeim.set_save_offline( True, base_offline_folder )
 my_mdeim.perform_mdeim( 20, 10**(-2) )
 
-my_mdeim.compute_theta_coefficients( param_min, 1 )
+my_mdeim.compute_theta_coefficients( param_min )
 
-#my_mdeim.print_reduced_indices( )
-#my_mdeim.print_reduced_indices_mat( )
-#my_mdeim.print_reduced_elements( )
+my_mdeim.print_reduced_indices( )
+my_mdeim.print_reduced_indices_mat( )
+my_mdeim.print_reduced_elements( )
+
+
+my_mdeim_2 = m_deim.Mdeim( my_ndp )
+my_mdeim_2.load_offline( base_offline_folder )
+my_mdeim_2.print_reduced_indices( )
+my_mdeim_2.print_reduced_indices_mat( )
+my_mdeim_2.print_reduced_elements( )
+my_mdeim_2.compute_theta_coefficients( param_min )
 
 A = my_ndp.assemble_fom_matrix( param_min )
 AA_mu_min = np.array(  A['A'] )
