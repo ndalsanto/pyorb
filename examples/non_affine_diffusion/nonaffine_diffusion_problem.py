cb9053e6deb500be102d47d6a23940b15352b345
Niccolo' Dal Santo <ncl.dalsanto@gmail.com>
2019-02-13 15:21:13 +0100

    updating example

--- examples/non_affine_diffusion/nonaffine_diffusion_problem.py
+++ examples/non_affine_diffusion/nonaffine_diffusion_problem.py
@@ -9,7 +9,7 @@ Created on Wed Oct 31 18:43:23 2018
 
 import pyorb_core.pde_problem.fom_problem as fp
 
-def ndp_theta_f( _param, _q ):
+def ndp_theta_f( _param, _q = 0 ):
     return 1.0
 
 class ndp_theta_fs( ):
@@ -18,6 +18,9 @@ class ndp_theta_fs( ):
     
     def ndp_theta_f( self, _param, _q ):
         return self.M_deim.compute_deim_theta_coefficients_q( _param, _q )
+    
+    def ndp_full_theta_f( self, _param ):
+        return self.M_deim.compute_deim_theta_coefficients( _param )
 
     def set_deim( self, _deim ):
         self.M_deim = _deim
@@ -32,6 +35,9 @@ class ndp_theta_As( ):
     
     def ndp_theta_a( self, _param, _q ):
         return self.M_mdeim.compute_theta_coefficients_q( _param, _q )
+
+    def ndp_full_theta_a( self, _param ):
+        return self.M_mdeim.compute_theta_coefficients( _param )
 
     def set_mdeim( self, _mdeim ):
         self.M_mdeim = _mdeim
@@ -55,11 +61,14 @@ class nonaffine_diffusion_problem( fp.fom_problem ):
     def define_theta_functions( self ):
         
         self.M_theta_a = self.M_ndp_theta_As.ndp_theta_a
+        self.M_full_theta_a = self.M_ndp_theta_As.ndp_full_theta_a
         
         if self.M_fom_specifics['use_nonhomogeneous_dirichlet'] == 'Y':
             self.M_theta_f = self.M_ndp_theta_fs.ndp_theta_f
+            self.M_full_theta_f = self.M_ndp_theta_fs.ndp_full_theta_f
         else:
             self.M_theta_f = ndp_theta_f
+            self.M_full_theta_f = self.M_ndp_theta_fs.ndp_full_theta_f
         
         return
     
