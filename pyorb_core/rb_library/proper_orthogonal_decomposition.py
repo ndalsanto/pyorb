81215faac30309df52cee2c9dbe0c3db260e0982
Niccolo' Dal Santo <ncl.dalsanto@gmail.com>
2019-03-12 13:23:05 +0100

    adding the computation of coefficients for snapshots

--- pyorb_core/rb_library/proper_orthogonal_decomposition.py
+++ pyorb_core/rb_library/proper_orthogonal_decomposition.py
@@ -19,9 +19,9 @@ class ProperOrthogonalDecompostion( ):
         self.M_snapshots_matrix = _snapshots_matrix
         self.M_ns = self.M_snapshots_matrix.shape[1]
     
-        U, s, V = np.linalg.svd( self.M_snapshots_matrix, full_matrices=False )
+        U, self.M_s, self.M_V = np.linalg.svd( self.M_snapshots_matrix, full_matrices=False )
         
-        total_energy = np.dot( s, np.transpose(s) )
+        total_energy = np.dot( self.M_s, np.transpose(self.M_s) )
         
         print( "The total energy of the field is %g" % total_energy )
         
@@ -32,21 +32,45 @@ class ProperOrthogonalDecompostion( ):
         
         while cumulative_energy / total_energy < 1. - _tol**2 and self.M_N < self.M_ns:
             print( "Now N is %d and the cumulative energy is %g --- %g --- current sv %g " \
-                                  % ( self.M_N, cumulative_energy, cumulative_energy / total_energy, s[ self.M_N ] ) )
+                                  % ( self.M_N, cumulative_energy, cumulative_energy / total_energy, self.M_s[ self.M_N ] ) )
 
             record_cumulative_energy[ self.M_N ] = cumulative_energy
-            cumulative_energy = cumulative_energy + s[ self.M_N ] * s[ self.M_N ]   # add the energy of next basis
+            cumulative_energy = cumulative_energy + self.M_s[ self.M_N ] * self.M_s[ self.M_N ]   # add the energy of next basis
             self.M_N = self.M_N + 1                                                 # add a basis in the count
 
         print( "Final N is %d" % self.M_N )
 
         self.M_basis = U[:, 0:self.M_N]
 
+        self.compute_snapshots_coefficient( )
+
     def get_basis( self ):
         return self.M_basis
+
+    def compute_snapshots_coefficient( self ):
+        
+        self.M_snapshots_coefficients = np.zeros( self.M_V.shape )
+
+        for ii in range( len(self.M_s) ):
+            self.M_snapshots_coefficients[ii, :] = self.M_s[ii] * self.M_V[ii, :]
+
+        return
+
+    def get_snapshots_coefficient( self ):
+        return self.M_snapshots_coefficients
 
     M_snapshots_matrix = np.zeros( ( 0, 0 ) )
     M_basis = np.zeros( ( 0, 0 ) )
     M_N = 0
     M_ns = 0
+    M_s = None
+    M_V = None
+    M_snapshots_coefficients= None
+    
+    
+    
+    
+    
+    
+    
     
