28ab3e833486ddcc738a20221c66b21deb45e921
Niccolo' Dal Santo <ncl.dalsanto@gmail.com>
2019-03-21 10:28:28 +0100

    implementing routines for solving NS RB nonlinear system

--- examples/navier_stokes/navier_stokes_problem.py
+++ examples/navier_stokes/navier_stokes_problem.py
@@ -21,7 +21,19 @@ class ns_theta_fs( ):
         return
     
     def ns_theta_f( self, _param, _q ):
-        return self.M_deim.compute_deim_theta_coefficients_q( _param, _q )
+
+        n_deim = self.M_deim.get_num_mdeim_basis( )
+
+        if _q >= 0 and _q < n_deim:
+            return self.M_deim.compute_deim_theta_coefficients_q( _param, _q )
+        
+        if _q == n_deim + 1:
+            return 1.0
+
+        # diffusion affine component
+        if _q == n_deim:
+            return 0.01 * _param[0]
+    
     
     def ns_full_theta_f( self, _param ):
         return self.M_deim.compute_deim_theta_coefficients( _param )
@@ -44,18 +56,23 @@ class ns_theta_As( ):
         if _q >= 0 and _q < n_m_deim:
             return self.M_mdeim.compute_theta_coefficients_q( _param, _q )
 
-        # diffusion affine component
         if _q == n_m_deim:
-            return _param[0]
+            return 1.0
+
+        # diffusion affine component
+        if _q == n_m_deim + 1:
+            return 0.01 * _param[0]
+
         
 
     def ns_full_theta_a( self, _param ):
         
         n_m_deim = self.M_mdeim.get_num_mdeim_basis( )
-        thetas = np.zeros( n_m_deim + 1 )
+        thetas = np.zeros( n_m_deim + 2 )
         
         thetas[0:n_m_deim] = self.M_mdeim.compute_theta_coefficients( _param )
         thetas[n_m_deim] = _param[0]
+        thetas[n_m_deim+1] = 1.0
 
     def set_mdeim( self, _mdeim ):
         self.M_mdeim = _mdeim
@@ -85,6 +102,59 @@ class navier_stokes_problem( fp.fom_problem ):
         self.M_full_theta_f = ns_full_theta_f
         
         return
+    
+    def build_ns_rb_jacobian( self, _uN, _rb_affine_decomposition, _theta_a ):
+        
+        ns_jac = np.zeros( _rb_affine_decomposition.get_rb_affine_matrix(0).shape )
+
+        for iQa in range( len(_theta_a) ):
+            ns_jac = ns_jac + _theta_a[iQa] * _rb_affine_decomposition.get_rb_affine_matrix( iQa )
+
+        for iQn in range( len(_uN) ):
+            ns_jac = ns_jac + _uN[iQn] * _rb_affine_decomposition.get_rb_affine_matrix( iQn + len(_theta_a) )
+
+        # the 2nd time is to include the flipped terms coming from the derivation
+        for iQn in range( len(_uN) ):
+            ns_jac = ns_jac + _uN[iQn] * _rb_affine_decomposition.get_rb_affine_matrix( iQn + len(_theta_a) + len(_uN) )
+        
+        return ns_jac
+
+    def build_ns_rb_matrix( self, _uN, _rb_affine_decomposition, _theta_a ):
+        
+        ns_rb_mat = np.zeros( _rb_affine_decomposition.get_rb_affine_matrix(0).shape )
+
+        print( '_theta_a.shape' )
+        print( _theta_a.shape )
+        print( _theta_a )
+
+        for iQa in range( len(_theta_a) ):
+            ns_rb_mat = ns_rb_mat + _theta_a[iQa] * _rb_affine_decomposition.get_rb_affine_matrix( iQa )
+        
+        for iQn in range( len(_uN) ):
+            ns_rb_mat = ns_rb_mat + _uN[iQn] * _rb_affine_decomposition.get_rb_affine_matrix( iQn + len(_theta_a) )
+
+        return ns_rb_mat
+
+    def build_ns_rb_vector( self, _theta_f, _rb_affine_decomposition ):
+        
+        ns_rb_rhs = np.zeros( _rb_affine_decomposition.get_rb_affine_vector(0).shape )
+
+        for iQf in range( len(_theta_f) ):
+            ns_rb_rhs = ns_rb_rhs + _theta_f[iQf] * _rb_affine_decomposition.get_rb_affine_vector( iQf )
+
+        return ns_rb_rhs
+
+    def rb_residual( self, _uN, _rb_affine_decomposition, _theta_f, _theta_a ):
+
+        ns_rb_mat = self.build_ns_rb_matrix( _uN, _rb_affine_decomposition, _theta_a )
+        ns_rb_rhs = self.build_ns_rb_vector( _theta_f, _rb_affine_decomposition )
+
+        res = ns_rb_mat.dot( _uN ) - ns_rb_rhs
+        
+        return res
+    
+    
+    
     
     M_ns_theta_As = ns_theta_As( )
     M_ns_theta_fs = ns_theta_fs( )
---
+++ pyorb_core/algorithms/newton_solver.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+
+def newton_solver( _jacobian, _residual, _u0, _tol, _n_max ):
+    
+    it = 0
+    u_n = _u0
+    
+    res = _residual( u_n )
+    res_norm = np.linalg.norm( res )
+    print( 'Starting residual norm is %e' % res_norm )
+    
+    while res_norm > _tol and it < _n_max:
+        it = it + 1
+        u_n_1 = 1.* u_n
+        u_n = u_n_1 - np.linalg.solve( _jacobian(u_n_1), _residual(u_n_1) )
+        res = _residual( u_n )
+        res_norm = np.linalg.norm( res )
+        print( 'Iteration %d residual norm is %e' % (it, res_norm) )
+    
+    return u_n
